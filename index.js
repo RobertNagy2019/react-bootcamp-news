ce48eaf882e71b8120471a308ebdfd4055e18a69
Robert Nagy <Robert.Nagy@ihse.de>
2019-09-10 15:54:36 +0200

    Abschnitt 10 - Lection 101: GET Stories and Nodemon

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const stories = require('./stories');
 
 const app = express();
 
 app.get('/ping', (req, res) => {
-    res.send('pong');
+    res.send('pong!');
+});
+
+app.get('/stories', (req, res) => {
+    res.json(stories);
+})
+
+app.get('/stories/:title', (req, res) => {
+    const { title } = req.params;
+
+    res.json(stories.filter(story => story.title.includes(title)));
+    // this will give on the page all stories with the title in witch contains the :"title" word
 });
 
 const PORT = 3000;
