9d3fce0dd9812d77ba9d319dac603ffbd4445f15
Robert Nagy <Robert.Nagy@ihse.de>
2019-09-11 08:04:15 +0200

    Abschnitt 10 -- Lection 104: Error Handler

--- index.js
+++ index.js
@@ -30,13 +30,22 @@ app.get('/stories/:title', (req, res) => {
     res.json(stories.filter(story => story.title.includes(title)));
 });
 
-app.get('/topstories', (req, res) => {
+app.get('/topstories', (req, res, next) => {
     request(
-        { url: 'https://hacker-news.firebaseio.com/v0/topstories.json' },
+        { url: 'https://hacker-news.firebaseio.com/v0/topstories.json' }, // to have an error change the url
         (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                console.log('going to next');
+                return next(new Error('Error requesting top stories'));
+            }
+
             res.json(JSON.parse(body)); // JSON.parse return an array instead a string
         }
     );
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ type: 'error', message: err.message });
 });
 
 const PORT = 3000;
